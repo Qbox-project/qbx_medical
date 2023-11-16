3bfe2fe07df63d2cf0fb4d8b3c29c589b3b1c881
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-11-16 01:10:28 -0500

    fix: manifest update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,29 +1,26 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'https://github.com/Qbox-project'
+description 'QBX_Medical'
+repository 'https://github.com/Qbox-project/qbx_medical'
 version '1.0.0'
 
 shared_scripts {
     '@ox_lib/init.lua',
     '@qbx_core/shared/locale.lua',
-    '@qbx_core/import.lua',
+    '@qbx_core/modules/utils.lua',
     'locales/en.lua',
     'locales/*.lua',
     'config.lua'
 }
 
-server_scripts {
-    'server/main.lua',
-}
-
 client_scripts {
+    '@qbx_core/modules/playerdata.lua',
     'client/**/*.lua',
 }
 
-modules {
-    'qbx_core:playerdata',
-    'qbx_core:utils'
+server_scripts {
+    'server/main.lua',
 }
 
 dependencies {

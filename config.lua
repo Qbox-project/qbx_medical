5f2a4aaf27c05a177959bee9a4bf42041c0495f5
unknown <clanerp@gmail.com>
2023-11-18 19:29:29 -0800

    refactor(config): use implied string keys

--- config.lua
+++ config.lua
@@ -39,70 +39,71 @@ Config.LegInjuryChance = { -- The chance, in percent, that leg injury side-effec
 Config.DeathTime = 300 -- How long the timer is for players to bleed out completely and respawn at the hospital
 Config.LaststandReviveInterval = 360
 
+---@enum WeaponClasse
 Config.WeaponClasses = { -- Define gta weapon classe numbers
-    ['SMALL_CALIBER'] = 1,
-    ['MEDIUM_CALIBER'] = 2,
-    ['HIGH_CALIBER'] = 3,
-    ['SHOTGUN'] = 4,
-    ['CUTTING'] = 5,
-    ['LIGHT_IMPACT'] = 6,
-    ['HEAVY_IMPACT'] = 7,
-    ['EXPLOSIVE'] = 8,
-    ['FIRE'] = 9,
-    ['SUFFOCATING'] = 10,
-    ['OTHER'] = 11,
-    ['WILDLIFE'] = 12,
-    ['NOTHING'] = 13
+    SMALL_CALIBER = 1,
+    MEDIUM_CALIBER = 2,
+    HIGH_CALIBER = 3,
+    SHOTGUN = 4,
+    CUTTING = 5,
+    LIGHT_IMPACT = 6,
+    HEAVY_IMPACT = 7,
+    EXPLOSIVE = 8,
+    FIRE = 9,
+    SUFFOCATING = 10,
+    OTHER = 11,
+    WILDLIFE = 12,
+    NOTHING = 13
 }
 
 Config.ForceInjury = 35 -- Maximum amount of damage a player can take before limb damage & effects are forced to occur
 Config.HealthDamage = 5 -- Minumum damage done to health before checking for injuries
 Config.MaxInjuryChanceMulti = 3 -- How many times the HealthDamage value above can divide into damage taken before damage is forced to be applied
 Config.ForceInjuryWeapons = { -- Define which weapons will always cause injuries
-    [Config.WeaponClasses['HIGH_CALIBER']] = true,
-    [Config.WeaponClasses['HEAVY_IMPACT']] = true,
-    [Config.WeaponClasses['EXPLOSIVE']] = true,
+    [Config.WeaponClasses.HIGH_CALIBER] = true,
+    [Config.WeaponClasses.HEAVY_IMPACT] = true,
+    [Config.WeaponClasses.EXPLOSIVE] = true,
 }
 
 Config.CriticalAreas = { -- Define body areas that will always cause bleeding if wearing armor or not
-    ['UPPER_BODY'] = { armored = false },
-    ['LOWER_BODY'] = { armored = true },
-    ['SPINE'] = { armored = true },
+    UPPER_BODY = { armored = false },
+    LOWER_BODY = { armored = true },
+    SPINE = { armored = true },
 }
 ---@class StaggerArea Defined body areas that will always cause staggering if wearing armor or not
 ---@field armored boolean
 ---@field major number
 ---@field minor number
 Config.StaggerAreas = {
-    ['SPINE'] = { armored = true, major = 60, minor = 30 },
-    ['UPPER_BODY'] = { armored = false, major = 60, minor = 30 },
-    ['LLEG'] = { armored = true, major = 100, minor = 85 },
-    ['RLEG'] = { armored = true, major = 100, minor = 85 },
-    ['LFOOT'] = { armored = true, major = 100, minor = 100 },
-    ['RFOOT'] = { armored = true, major = 100, minor = 100 },
+    SPINE = { armored = true, major = 60, minor = 30 },
+    UPPER_BODY = { armored = false, major = 60, minor = 30 },
+    LLEG = { armored = true, major = 100, minor = 85 },
+    RLEG = { armored = true, major = 100, minor = 85 },
+    LFOOT = { armored = true, major = 100, minor = 100 },
+    RFOOT = { armored = true, major = 100, minor = 100 },
 }
 Config.MajorArmoredBleedChance = 45 -- The chance, in percent, that a player will get a bleed effect when taking heavy damage while wearing armor
 Config.MinorInjurWeapons = { -- Define which weapons cause small injuries
-    [Config.WeaponClasses['SMALL_CALIBER']] = true,
-    [Config.WeaponClasses['MEDIUM_CALIBER']] = true,
-    [Config.WeaponClasses['CUTTING']] = true,
-    [Config.WeaponClasses['WILDLIFE']] = true,
-    [Config.WeaponClasses['OTHER']] = true,
-    [Config.WeaponClasses['LIGHT_IMPACT']] = true,
+    [Config.WeaponClasses.SMALL_CALIBER] = true,
+    [Config.WeaponClasses.MEDIUM_CALIBER] = true,
+    [Config.WeaponClasses.CUTTING] = true,
+    [Config.WeaponClasses.WILDLIFE] = true,
+    [Config.WeaponClasses.OTHER] = true,
+    [Config.WeaponClasses.LIGHT_IMPACT] = true,
 }
 
 Config.MajorInjurWeapons = { -- Define which weapons cause large injuries
-    [Config.WeaponClasses['HIGH_CALIBER']] = true,
-    [Config.WeaponClasses['HEAVY_IMPACT']] = true,
-    [Config.WeaponClasses['SHOTGUN']] = true,
-    [Config.WeaponClasses['EXPLOSIVE']] = true,
+    [Config.WeaponClasses.HIGH_CALIBER] = true,
+    [Config.WeaponClasses.HEAVY_IMPACT] = true,
+    [Config.WeaponClasses.SHOTGUN] = true,
+    [Config.WeaponClasses.EXPLOSIVE] = true,
 }
 Config.DamageMinorToMajor = 35 -- How much damage would have to be applied for a minor weapon to be considered a major damage event. Put this at 100 if you want to disable it
 Config.AlwaysBleedChanceWeapons = { -- Define which weapons will always cause bleedign
-    [Config.WeaponClasses['SMALL_CALIBER']] = true,
-    [Config.WeaponClasses['MEDIUM_CALIBER']] = true,
-    [Config.WeaponClasses['CUTTING']] = true,
-    [Config.WeaponClasses['WILDLIFE']] = false,
+    [Config.WeaponClasses.SMALL_CALIBER] = true,
+    [Config.WeaponClasses.MEDIUM_CALIBER] = true,
+    [Config.WeaponClasses.CUTTING] = true,
+    [Config.WeaponClasses.WILDLIFE] = false,
 }
 Config.AlwaysBleedChance = 70 -- Set the chance out of 100 that if a player is hit with a weapon, that also has a random chance, it will cause bleeding
 
@@ -166,123 +167,123 @@ Config.Bones = { -- Correspond bone hash numbers to their label
 }
 
 Config.Weapons = { -- Correspond weapon names to their class number
-    [`WEAPON_STUNGUN`] = Config.WeaponClasses['NONE'],
-    [`WEAPON_STUNGUN_MP`] = Config.WeaponClasses['NONE'],
+    [`WEAPON_STUNGUN`] = Config.WeaponClasses.NONE,
+    [`WEAPON_STUNGUN_MP`] = Config.WeaponClasses.NONE,
     --[[ Small Caliber ]] --
-    [`WEAPON_PISTOL`] = Config.WeaponClasses['SMALL_CALIBER'],
-    [`WEAPON_COMBATPISTOL`] = Config.WeaponClasses['SMALL_CALIBER'],
-    [`WEAPON_APPISTOL`] = Config.WeaponClasses['SMALL_CALIBER'],
-    [`WEAPON_COMBATPDW`] = Config.WeaponClasses['SMALL_CALIBER'],
-    [`WEAPON_MACHINEPISTOL`] = Config.WeaponClasses['SMALL_CALIBER'],
-    [`WEAPON_MICROSMG`] = Config.WeaponClasses['SMALL_CALIBER'],
-    [`WEAPON_MINISMG`] = Config.WeaponClasses['SMALL_CALIBER'],
-    [`WEAPON_PISTOL_MK2`] = Config.WeaponClasses['SMALL_CALIBER'],
-    [`WEAPON_SNSPISTOL`] = Config.WeaponClasses['SMALL_CALIBER'],
-    [`WEAPON_SNSPISTOL_MK2`] = Config.WeaponClasses['SMALL_CALIBER'],
-    [`WEAPON_VINTAGEPISTOL`] = Config.WeaponClasses['SMALL_CALIBER'],
+    [`WEAPON_PISTOL`] = Config.WeaponClasses.SMALL_CALIBER,
+    [`WEAPON_COMBATPISTOL`] = Config.WeaponClasses.SMALL_CALIBER,
+    [`WEAPON_APPISTOL`] = Config.WeaponClasses.SMALL_CALIBER,
+    [`WEAPON_COMBATPDW`] = Config.WeaponClasses.SMALL_CALIBER,
+    [`WEAPON_MACHINEPISTOL`] = Config.WeaponClasses.SMALL_CALIBER,
+    [`WEAPON_MICROSMG`] = Config.WeaponClasses.SMALL_CALIBER,
+    [`WEAPON_MINISMG`] = Config.WeaponClasses.SMALL_CALIBER,
+    [`WEAPON_PISTOL_MK2`] = Config.WeaponClasses.SMALL_CALIBER,
+    [`WEAPON_SNSPISTOL`] = Config.WeaponClasses.SMALL_CALIBER,
+    [`WEAPON_SNSPISTOL_MK2`] = Config.WeaponClasses.SMALL_CALIBER,
+    [`WEAPON_VINTAGEPISTOL`] = Config.WeaponClasses.SMALL_CALIBER,
 
     --[[ Medium Caliber ]] --
-    [`WEAPON_ADVANCEDRIFLE`] = Config.WeaponClasses['MEDIUM_CALIBER'],
-    [`WEAPON_ASSAULTSMG`] = Config.WeaponClasses['MEDIUM_CALIBER'],
-    [`WEAPON_BULLPUPRIFLE`] = Config.WeaponClasses['MEDIUM_CALIBER'],
-    [`WEAPON_BULLPUPRIFLE_MK2`] = Config.WeaponClasses['MEDIUM_CALIBER'],
-    [`WEAPON_CARBINERIFLE`] = Config.WeaponClasses['MEDIUM_CALIBER'],
-    [`WEAPON_CARBINERIFLE_MK2`] = Config.WeaponClasses['MEDIUM_CALIBER'],
-    [`WEAPON_COMPACTRIFLE`] = Config.WeaponClasses['MEDIUM_CALIBER'],
-    [`WEAPON_DOUBLEACTION`] = Config.WeaponClasses['MEDIUM_CALIBER'],
-    [`WEAPON_GUSENBERG`] = Config.WeaponClasses['MEDIUM_CALIBER'],
-    [`WEAPON_HEAVYPISTOL`] = Config.WeaponClasses['MEDIUM_CALIBER'],
-    [`WEAPON_MARKSMANPISTOL`] = Config.WeaponClasses['MEDIUM_CALIBER'],
-    [`WEAPON_PISTOL50`] = Config.WeaponClasses['MEDIUM_CALIBER'],
-    [`WEAPON_REVOLVER`] = Config.WeaponClasses['MEDIUM_CALIBER'],
-    [`WEAPON_REVOLVER_MK2`] = Config.WeaponClasses['MEDIUM_CALIBER'],
-    [`WEAPON_SMG`] = Config.WeaponClasses['MEDIUM_CALIBER'],
-    [`WEAPON_SMG_MK2`] = Config.WeaponClasses['MEDIUM_CALIBER'],
-    [`WEAPON_SPECIALCARBINE`] = Config.WeaponClasses['MEDIUM_CALIBER'],
-    [`WEAPON_SPECIALCARBINE_MK2`] = Config.WeaponClasses['MEDIUM_CALIBER'],
+    [`WEAPON_ADVANCEDRIFLE`] = Config.WeaponClasses.MEDIUM_CALIBER,
+    [`WEAPON_ASSAULTSMG`] = Config.WeaponClasses.MEDIUM_CALIBER,
+    [`WEAPON_BULLPUPRIFLE`] = Config.WeaponClasses.MEDIUM_CALIBER,
+    [`WEAPON_BULLPUPRIFLE_MK2`] = Config.WeaponClasses.MEDIUM_CALIBER,
+    [`WEAPON_CARBINERIFLE`] = Config.WeaponClasses.MEDIUM_CALIBER,
+    [`WEAPON_CARBINERIFLE_MK2`] = Config.WeaponClasses.MEDIUM_CALIBER,
+    [`WEAPON_COMPACTRIFLE`] = Config.WeaponClasses.MEDIUM_CALIBER,
+    [`WEAPON_DOUBLEACTION`] = Config.WeaponClasses.MEDIUM_CALIBER,
+    [`WEAPON_GUSENBERG`] = Config.WeaponClasses.MEDIUM_CALIBER,
+    [`WEAPON_HEAVYPISTOL`] = Config.WeaponClasses.MEDIUM_CALIBER,
+    [`WEAPON_MARKSMANPISTOL`] = Config.WeaponClasses.MEDIUM_CALIBER,
+    [`WEAPON_PISTOL50`] = Config.WeaponClasses.MEDIUM_CALIBER,
+    [`WEAPON_REVOLVER`] = Config.WeaponClasses.MEDIUM_CALIBER,
+    [`WEAPON_REVOLVER_MK2`] = Config.WeaponClasses.MEDIUM_CALIBER,
+    [`WEAPON_SMG`] = Config.WeaponClasses.MEDIUM_CALIBER,
+    [`WEAPON_SMG_MK2`] = Config.WeaponClasses.MEDIUM_CALIBER,
+    [`WEAPON_SPECIALCARBINE`] = Config.WeaponClasses.MEDIUM_CALIBER,
+    [`WEAPON_SPECIALCARBINE_MK2`] = Config.WeaponClasses.MEDIUM_CALIBER,
 
     --[[ High Caliber ]] --
-    [`WEAPON_ASSAULTRIFLE`] = Config.WeaponClasses['HIGH_CALIBER'],
-    [`WEAPON_ASSAULTRIFLE_MK2`] = Config.WeaponClasses['HIGH_CALIBER'],
-    [`WEAPON_COMBATMG`] = Config.WeaponClasses['HIGH_CALIBER'],
-    [`WEAPON_COMBATMG_MK2`] = Config.WeaponClasses['HIGH_CALIBER'],
-    [`WEAPON_HEAVYSNIPER`] = Config.WeaponClasses['HIGH_CALIBER'],
-    [`WEAPON_HEAVYSNIPER_MK2`] = Config.WeaponClasses['HIGH_CALIBER'],
-    [`WEAPON_MARKSMANRIFLE`] = Config.WeaponClasses['HIGH_CALIBER'],
-    [`WEAPON_MARKSMANRIFLE_MK2`] = Config.WeaponClasses['HIGH_CALIBER'],
-    [`WEAPON_MG`] = Config.WeaponClasses['HIGH_CALIBER'],
-    [`WEAPON_MINIGUN`] = Config.WeaponClasses['HIGH_CALIBER'],
-    [`WEAPON_MUSKET`] = Config.WeaponClasses['HIGH_CALIBER'],
-    [`WEAPON_RAILGUN`] = Config.WeaponClasses['HIGH_CALIBER'],
-    [`WEAPON_HEAVYRIFLE`] = Config.WeaponClasses['HIGH_CALIBER'],
+    [`WEAPON_ASSAULTRIFLE`] = Config.WeaponClasses.HIGH_CALIBER,
+    [`WEAPON_ASSAULTRIFLE_MK2`] = Config.WeaponClasses.HIGH_CALIBER,
+    [`WEAPON_COMBATMG`] = Config.WeaponClasses.HIGH_CALIBER,
+    [`WEAPON_COMBATMG_MK2`] = Config.WeaponClasses.HIGH_CALIBER,
+    [`WEAPON_HEAVYSNIPER`] = Config.WeaponClasses.HIGH_CALIBER,
+    [`WEAPON_HEAVYSNIPER_MK2`] = Config.WeaponClasses.HIGH_CALIBER,
+    [`WEAPON_MARKSMANRIFLE`] = Config.WeaponClasses.HIGH_CALIBER,
+    [`WEAPON_MARKSMANRIFLE_MK2`] = Config.WeaponClasses.HIGH_CALIBER,
+    [`WEAPON_MG`] = Config.WeaponClasses.HIGH_CALIBER,
+    [`WEAPON_MINIGUN`] = Config.WeaponClasses.HIGH_CALIBER,
+    [`WEAPON_MUSKET`] = Config.WeaponClasses.HIGH_CALIBER,
+    [`WEAPON_RAILGUN`] = Config.WeaponClasses.HIGH_CALIBER,
+    [`WEAPON_HEAVYRIFLE`] = Config.WeaponClasses.HIGH_CALIBER,
 
     --[[ Shotguns ]] --
-    [`WEAPON_ASSAULTSHOTGUN`] = Config.WeaponClasses['SHOTGUN'],
-    [`WEAPON_BULLUPSHOTGUN`] = Config.WeaponClasses['SHOTGUN'],
-    [`WEAPON_DBSHOTGUN`] = Config.WeaponClasses['SHOTGUN'],
-    [`WEAPON_HEAVYSHOTGUN`] = Config.WeaponClasses['SHOTGUN'],
-    [`WEAPON_PUMPSHOTGUN`] = Config.WeaponClasses['SHOTGUN'],
-    [`WEAPON_PUMPSHOTGUN_MK2`] = Config.WeaponClasses['SHOTGUN'],
-    [`WEAPON_SAWNOFFSHOTGUN`] = Config.WeaponClasses['SHOTGUN'],
-    [`WEAPON_SWEEPERSHOTGUN`] = Config.WeaponClasses['SHOTGUN'],
+    [`WEAPON_ASSAULTSHOTGUN`] = Config.WeaponClasses.SHOTGUN,
+    [`WEAPON_BULLUPSHOTGUN`] = Config.WeaponClasses.SHOTGUN,
+    [`WEAPON_DBSHOTGUN`] = Config.WeaponClasses.SHOTGUN,
+    [`WEAPON_HEAVYSHOTGUN`] = Config.WeaponClasses.SHOTGUN,
+    [`WEAPON_PUMPSHOTGUN`] = Config.WeaponClasses.SHOTGUN,
+    [`WEAPON_PUMPSHOTGUN_MK2`] = Config.WeaponClasses.SHOTGUN,
+    [`WEAPON_SAWNOFFSHOTGUN`] = Config.WeaponClasses.SHOTGUN,
+    [`WEAPON_SWEEPERSHOTGUN`] = Config.WeaponClasses.SHOTGUN,
 
     --[[ Animals ]] --
-    [`WEAPON_ANIMAL`] = Config.WeaponClasses['WILDLIFE'], -- Animal
-    [`WEAPON_COUGAR`] = Config.WeaponClasses['WILDLIFE'], -- Cougar
-    [`WEAPON_BARBED_WIRE`] = Config.WeaponClasses['WILDLIFE'], -- Barbed Wire
+    [`WEAPON_ANIMAL`] = Config.WeaponClasses.WILDLIFE, -- Animal
+    [`WEAPON_COUGAR`] = Config.WeaponClasses.WILDLIFE, -- Cougar
+    [`WEAPON_BARBED_WIRE`] = Config.WeaponClasses.WILDLIFE, -- Barbed Wire
 
     --[[ Cutting Weapons ]] --
-    [`WEAPON_BATTLEAXE`] = Config.WeaponClasses['CUTTING'],
-    [`WEAPON_BOTTLE`] = Config.WeaponClasses['CUTTING'],
-    [`WEAPON_DAGGER`] = Config.WeaponClasses['CUTTING'],
-    [`WEAPON_HATCHET`] = Config.WeaponClasses['CUTTING'],
-    [`WEAPON_KNIFE`] = Config.WeaponClasses['CUTTING'],
-    [`WEAPON_MACHETE`] = Config.WeaponClasses['CUTTING'],
-    [`WEAPON_SWITCHBLADE`] = Config.WeaponClasses['CUTTING'],
+    [`WEAPON_BATTLEAXE`] = Config.WeaponClasses.CUTTING,
+    [`WEAPON_BOTTLE`] = Config.WeaponClasses.CUTTING,
+    [`WEAPON_DAGGER`] = Config.WeaponClasses.CUTTING,
+    [`WEAPON_HATCHET`] = Config.WeaponClasses.CUTTING,
+    [`WEAPON_KNIFE`] = Config.WeaponClasses.CUTTING,
+    [`WEAPON_MACHETE`] = Config.WeaponClasses.CUTTING,
+    [`WEAPON_SWITCHBLADE`] = Config.WeaponClasses.CUTTING,
 
     --[[ Light Impact ]] --
-    [`WEAPON_KNUCKLE`] = Config.WeaponClasses['LIGHT_IMPACT'],
+    [`WEAPON_KNUCKLE`] = Config.WeaponClasses.LIGHT_IMPACT,
 
     --[[ Heavy Impact ]] --
-    [`WEAPON_BAT`] = Config.WeaponClasses['HEAVY_IMPACT'],
-    [`WEAPON_CROWBAR`] = Config.WeaponClasses['HEAVY_IMPACT'],
-    [`WEAPON_FIREEXTINGUISHER`] = Config.WeaponClasses['HEAVY_IMPACT'],
-    [`WEAPON_FIRWORK`] = Config.WeaponClasses['HEAVY_IMPACT'],
-    [`WEAPON_GOLFLCUB`] = Config.WeaponClasses['HEAVY_IMPACT'],
-    [`WEAPON_HAMMER`] = Config.WeaponClasses['HEAVY_IMPACT'],
-    [`WEAPON_PETROLCAN`] = Config.WeaponClasses['HEAVY_IMPACT'],
-    [`WEAPON_POOLCUE`] = Config.WeaponClasses['HEAVY_IMPACT'],
-    [`WEAPON_WRENCH`] = Config.WeaponClasses['HEAVY_IMPACT'],
-    [`WEAPON_RAMMED_BY_CAR`] = Config.WeaponClasses['HEAVY_IMPACT'],
-    [`WEAPON_RUN_OVER_BY_CAR`] = Config.WeaponClasses['HEAVY_IMPACT'],
+    [`WEAPON_BAT`] = Config.WeaponClasses.HEAVY_IMPACT,
+    [`WEAPON_CROWBAR`] = Config.WeaponClasses.HEAVY_IMPACT,
+    [`WEAPON_FIREEXTINGUISHER`] = Config.WeaponClasses.HEAVY_IMPACT,
+    [`WEAPON_FIRWORK`] = Config.WeaponClasses.HEAVY_IMPACT,
+    [`WEAPON_GOLFLCUB`] = Config.WeaponClasses.HEAVY_IMPACT,
+    [`WEAPON_HAMMER`] = Config.WeaponClasses.HEAVY_IMPACT,
+    [`WEAPON_PETROLCAN`] = Config.WeaponClasses.HEAVY_IMPACT,
+    [`WEAPON_POOLCUE`] = Config.WeaponClasses.HEAVY_IMPACT,
+    [`WEAPON_WRENCH`] = Config.WeaponClasses.HEAVY_IMPACT,
+    [`WEAPON_RAMMED_BY_CAR`] = Config.WeaponClasses.HEAVY_IMPACT,
+    [`WEAPON_RUN_OVER_BY_CAR`] = Config.WeaponClasses.HEAVY_IMPACT,
 
     --[[ Explosives ]] --
-    [`WEAPON_EXPLOSION`] = Config.WeaponClasses['EXPLOSIVE'],
-    [`WEAPON_GRENADE`] = Config.WeaponClasses['EXPLOSIVE'],
-    [`WEAPON_COMPACTLAUNCHER`] = Config.WeaponClasses['EXPLOSIVE'],
-    [`WEAPON_HOMINGLAUNCHER`] = Config.WeaponClasses['EXPLOSIVE'],
-    [`WEAPON_PIPEBOMB`] = Config.WeaponClasses['EXPLOSIVE'],
-    [`WEAPON_PROXMINE`] = Config.WeaponClasses['EXPLOSIVE'],
-    [`WEAPON_RPG`] = Config.WeaponClasses['EXPLOSIVE'],
-    [`WEAPON_STICKYBOMB`] = Config.WeaponClasses['EXPLOSIVE'],
-    [`WEAPON_HELI_CRASH`] = Config.WeaponClasses['EXPLOSIVE'],
-    [`WEAPON_EMPLAUNCHER`] = Config.WeaponClasses['EXPLOSIVE'],
+    [`WEAPON_EXPLOSION`] = Config.WeaponClasses.EXPLOSIVE,
+    [`WEAPON_GRENADE`] = Config.WeaponClasses.EXPLOSIVE,
+    [`WEAPON_COMPACTLAUNCHER`] = Config.WeaponClasses.EXPLOSIVE,
+    [`WEAPON_HOMINGLAUNCHER`] = Config.WeaponClasses.EXPLOSIVE,
+    [`WEAPON_PIPEBOMB`] = Config.WeaponClasses.EXPLOSIVE,
+    [`WEAPON_PROXMINE`] = Config.WeaponClasses.EXPLOSIVE,
+    [`WEAPON_RPG`] = Config.WeaponClasses.EXPLOSIVE,
+    [`WEAPON_STICKYBOMB`] = Config.WeaponClasses.EXPLOSIVE,
+    [`WEAPON_HELI_CRASH`] = Config.WeaponClasses.EXPLOSIVE,
+    [`WEAPON_EMPLAUNCHER`] = Config.WeaponClasses.EXPLOSIVE,
 
     --[[ Other ]] --
-    [`WEAPON_FALL`] = Config.WeaponClasses['OTHER'], -- Fall
-    [`WEAPON_HIT_BY_WATER_CANNON`] = Config.WeaponClasses['OTHER'], -- Water Cannon
+    [`WEAPON_FALL`] = Config.WeaponClasses.OTHER, -- Fall
+    [`WEAPON_HIT_BY_WATER_CANNON`] = Config.WeaponClasses.OTHER, -- Water Cannon
 
     --[[ Fire ]] --
-    [`WEAPON_ELECTRIC_FENCE`] = Config.WeaponClasses['FIRE'],
-    [`WEAPON_FIRE`] = Config.WeaponClasses['FIRE'],
-    [`WEAPON_MOLOTOV`] = Config.WeaponClasses['FIRE'],
-    [`WEAPON_FLARE`] = Config.WeaponClasses['FIRE'],
-    [`WEAPON_FLAREGUN`] = Config.WeaponClasses['FIRE'],
+    [`WEAPON_ELECTRIC_FENCE`] = Config.WeaponClasses.FIRE,
+    [`WEAPON_FIRE`] = Config.WeaponClasses.FIRE,
+    [`WEAPON_MOLOTOV`] = Config.WeaponClasses.FIRE,
+    [`WEAPON_FLARE`] = Config.WeaponClasses.FIRE,
+    [`WEAPON_FLAREGUN`] = Config.WeaponClasses.FIRE,
 
     --[[ Suffocate ]] --
-    [`WEAPON_DROWNING`] = Config.WeaponClasses['SUFFOCATING'], -- Drowning
-    [`WEAPON_DROWNING_IN_VEHICLE`] = Config.WeaponClasses['SUFFOCATING'], -- Drowning Veh
-    [`WEAPON_EXHAUSTION`] = Config.WeaponClasses['SUFFOCATING'], -- Exhaust
-    [`WEAPON_BZGAS`] = Config.WeaponClasses['SUFFOCATING'],
-    [`WEAPON_SMOKEGRENADE`] = Config.WeaponClasses['SUFFOCATING'],
+    [`WEAPON_DROWNING`] = Config.WeaponClasses.SUFFOCATING, -- Drowning
+    [`WEAPON_DROWNING_IN_VEHICLE`] = Config.WeaponClasses.SUFFOCATING, -- Drowning Veh
+    [`WEAPON_EXHAUSTION`] = Config.WeaponClasses.SUFFOCATING, -- Exhaust
+    [`WEAPON_BZGAS`] = Config.WeaponClasses.SUFFOCATING,
+    [`WEAPON_SMOKEGRENADE`] = Config.WeaponClasses.SUFFOCATING,
 }

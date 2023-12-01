0812433b35692fb7ee346f605f87bf0d9e1261c7
Reyghita Hafizh <87755064+reyyghi@users.noreply.github.com>
2023-12-01 14:35:04 +0700

    fix: timeout waiting for bed to stop moving to prevent small jitter from keeping player from going into last stand
    * Fixes bug (0 HP but not really dead)
    
    * Update laststand.lua

--- client/laststand.lua
+++ client/laststand.lua
@@ -5,7 +5,8 @@ local sharedConfig = require 'config.shared'
 ---@param ped number
 function WaitForPlayerToStopMoving()
     local ped = cache.ped
-    while GetEntitySpeed(ped) > 0.5 or IsPedRagdoll(ped) do Wait(10) end
+    local TimeOut = 10000
+    while GetEntitySpeed(ped) > 0.5 or IsPedRagdoll(ped) and TimeOut > 1 do TimeOut -= 10 Wait(10) end
 end
 
 --- low level GTA resurrection
